4b59b9dfffe7abc36e198be2de7ce84abbd46fd3
Muskaanpal05 <palmuskaan5@gmail.com>
2020-01-13 11:00:52 +0530

    nba project working now

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
-import Teams from "./components/Teams";
-import Games from "./components/Games";
-// import Typography from 'Typography';
+import NBA_Teams from "./components/nba_teams";
+import NBA_Games from "./components/nba_games";
 import { Toolbar, Grid, withStyles,Typography, Tabs, Tab, Paper } from '@material-ui/core';
 
-
 const style = {
-  heading: {
+  main_title: {
     width: "100%",
+    color: "#f16d32",
     textAlign: "center",
-    marginTop: "5rem",
-    color: "#f16d32"
+    marginTop: "5rem"
   },
-  tabSelected: {
+  selection: {
     background: "#f16d32",
     color: "white"
   }
@@ -21,13 +19,13 @@ const style = {
 function App(props) {
   const { classes } = props;
   const [currentTab, setCurrentTab] = useState(0);
-  function handleTabChange(e, val) {
+  function handellingChange(e, val) {
     setCurrentTab(val);
   }
   return (
     <div className="App">
       <Toolbar>
-        <Grid container justify="center" className={classes.heading}>
+        <Grid container justify="center" className={classes.main_title}>
           <Typography variant="h3" color="inherit">
             NBA REPO
           </Typography>
@@ -38,19 +36,19 @@ function App(props) {
           <Paper>
           <Tabs
             value={currentTab}
-            onChange={handleTabChange}
+            onChange={handellingChange}
             variant="fullWidth"
             centered
             style={{ width: "100%" }}
           >
-            <Tab label="NBA Teams" classes={{ selected: classes.tabSelected }} />
-            <Tab label="NBA Games" classes={{ selected: classes.tabSelected }} />
+            <Tab label="NBA Teams" classes={{ selected: classes.selection }} />
+            <Tab label="NBA Games" classes={{ selected: classes.selection }} />
           </Tabs>
           </Paper>
         </Grid>
         <Grid item xs={12} lg={9} md={10}>
           {
-            currentTab === 0?<Teams></Teams>:<Games></Games>
+            currentTab === 0?<NBA_Teams></NBA_Teams>:<NBA_Games></NBA_Games>
           }
         </Grid>
       </Grid>
